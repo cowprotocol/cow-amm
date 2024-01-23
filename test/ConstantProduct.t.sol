01e68f420f65bdf64049518b82e578ea18f8f78f
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-01-23 15:07:15 +0100

    Add test infrastructure

--- test/ConstantProduct.t.sol
+++ test/ConstantProduct.t.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
+import {IERC20} from "lib/composable-cow/lib/@openzeppelin/contracts/interfaces/IERC20.sol";
+import {BaseComposableCoWTest} from "lib/composable-cow/test/ComposableCoW.base.t.sol";
+
 import {ConstantProduct} from "../src/ConstantProduct.sol";
 
-contract CounterTest is Test {}
+contract E2eCounterTest is BaseComposableCoWTest {
+    ConstantProduct constantProduct;
+    address safe;
+
+    function setUp() public virtual override(BaseComposableCoWTest) {
+        super.setUp();
+
+        constantProduct = new ConstantProduct();
+    }
+}
+
