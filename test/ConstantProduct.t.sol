d975d217796aa1363180e5df16d8f00bb32678e3
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-01-25 19:12:55 +0100

    Remove current logic

--- test/ConstantProduct.t.sol
+++ test/ConstantProduct.t.sol
@@ -1,19 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IERC20} from "lib/composable-cow/lib/@openzeppelin/contracts/interfaces/IERC20.sol";
-import {BaseComposableCoWTest} from "lib/composable-cow/test/ComposableCoW.base.t.sol";
-
-import {ConstantProduct} from "../src/ConstantProduct.sol";
-
-contract E2eCounterTest is BaseComposableCoWTest {
-    ConstantProduct constantProduct;
-    address safe;
-
-    function setUp() public virtual override(BaseComposableCoWTest) {
-        super.setUp();
-
-        constantProduct = new ConstantProduct();
-    }
-}
-
+contract E2eCounterTest {}
