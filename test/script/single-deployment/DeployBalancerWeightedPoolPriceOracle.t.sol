9a1523783a7f7c5b9c875b56c8b566e50ebe9ea5
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-02-13 16:52:55 +0000

    Add Balancer weighted pool oracle deployment script (#17)
    * Add balancer price oracle
    
    * Add Balancer weighted pool oracle deployment script

---
+++ test/script/single-deployment/DeployBalancerWeightedPoolPriceOracle.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import {Test} from "forge-std/Test.sol";
+
+import {BalancerSetUp} from "./balancer/BalancerSetUp.sol";
+
+import {DeployBalancerWeightedPoolPriceOracle} from "script/single-deployment/BalancerWeightedPoolPriceOracle.s.sol";
+
+contract DeployBalancerWeightedPoolPriceOracleTest is Test, BalancerSetUp {
+    DeployBalancerWeightedPoolPriceOracle script;
+
+    function setUp() public {
+        setUpBalancerVault();
+        script = new DeployBalancerWeightedPoolPriceOracle();
+    }
+
+    function testDoesNotRevert() public {
+        script.run();
+    }
+}
