1c0c22976fc8198293ce69e5aad5e4bc9109e043
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-02-13 15:59:28 +0100

    Add Balancer weighted pool oracle deployment script

---
+++ test/script/single-deployment/balancer/BalancerSetUp.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import {Test} from "forge-std/Test.sol";
+
+abstract contract BalancerSetUp is Test {
+    function setUpBalancerVault() internal {
+        vm.etch(0xBA12222222228d8Ba445958a75a0704d566BF2C8, hex"1337");
+    }
+}
