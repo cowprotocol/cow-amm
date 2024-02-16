1ea1e651f66181cabda3a1285e1f1b21e634070f
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-02-16 10:01:38 +0100

    Add dedicated test to ContractProduct tests

--- test/ConstantProduct.t.sol
+++ test/ConstantProduct.t.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.13;
 import {VerifyTest} from "./ConstantProduct/VerifyTest.sol";
 import {GetTradeableOrderTest} from "./ConstantProduct/GetTradeableOrderTest.sol";
 import {CommitTest} from "./ConstantProduct/CommitTest.sol";
+import {DeploymentParamsTest} from "./ConstantProduct/DeploymentParamsTest.sol";
 
-contract ConstantProductTest is VerifyTest, GetTradeableOrderTest, CommitTest {}
+contract ConstantProductTest is VerifyTest, GetTradeableOrderTest, CommitTest, DeploymentParamsTest {}
