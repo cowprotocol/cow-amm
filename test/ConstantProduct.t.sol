145384cca64c44dd36bf68ae874f172b302eba10
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-02-16 09:50:03 +0000

    Add tests for `supportsInterface` (#22)
    * Add tests for `supportsInterface`
    
    * Fully capitalize IERC165 initialism in contract name
    
    Fully capitalizing is consistent with the [solidity style guide](https://docs.soliditylang.org/en/latest/style-guide.html#naming-styles).
    
    Co-authored-by: mfw78 <53399572+mfw78@users.noreply.github.com>
    
    * Rename file using uppercase initialism
    
    ---------
    
    Co-authored-by: mfw78 <53399572+mfw78@users.noreply.github.com>

--- test/ConstantProduct.t.sol
+++ test/ConstantProduct.t.sol
@@ -5,5 +5,6 @@ import {VerifyTest} from "./ConstantProduct/VerifyTest.sol";
 import {GetTradeableOrderTest} from "./ConstantProduct/GetTradeableOrderTest.sol";
 import {CommitTest} from "./ConstantProduct/CommitTest.sol";
 import {DeploymentParamsTest} from "./ConstantProduct/DeploymentParamsTest.sol";
+import {IERC165Test} from "./ConstantProduct/IERC165Test.sol";
 
-contract ConstantProductTest is VerifyTest, GetTradeableOrderTest, CommitTest, DeploymentParamsTest {}
+contract ConstantProductTest is VerifyTest, GetTradeableOrderTest, CommitTest, DeploymentParamsTest, IERC165Test {}
