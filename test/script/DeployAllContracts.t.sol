b3300b035a0e0ed3f70deb7ece954be5dc5b3f84
mfw78 <mfw78@protonmail.com>
2024-02-25 11:09:20 +0000

    fix: tests

--- test/script/DeployAllContracts.t.sol
+++ test/script/DeployAllContracts.t.sol
@@ -5,15 +5,18 @@ import {Test} from "forge-std/Test.sol";
 
 import {BalancerSetUp} from "./single-deployment/balancer/BalancerSetUp.sol";
 import {CowProtocolSetUp} from "./single-deployment/cow-protocol/CowProtocolSetUp.sol";
+import {SafeSetUp} from "./single-deployment/safe/SafeSetUp.sol";
 
 import {DeployAllContracts} from "script/DeployAllContracts.s.sol";
 
-contract DeployAllContractsTest is Test, BalancerSetUp, CowProtocolSetUp {
+contract DeployAllContractsTest is Test, BalancerSetUp, CowProtocolSetUp, SafeSetUp {
     DeployAllContracts script;
 
     function setUp() public {
         setUpSettlementContract();
+        setUpComposableCowContract();
         setUpBalancerVault();
+        setUpSafeExtensibleFallbackHandler();
         script = new DeployAllContracts();
     }
 
