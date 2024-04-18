850de84e75dbcf09e32bb126ea603d2218acb3b3
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-04-18 06:28:05 +0000

    Add view function to get tradeable order (#49)
    Part of making CoW AMM standalone
    (https://github.com/cowprotocol/cow-amm/pull/44).
    
    `ConstantProductFactory` takes the role of the `ComposableCoW` contract
    for creating conditional orders.
    For now, this means that the watchtower can query
    `getTradeableOrderWithSignature` as if it were `ComposableCoW`.
    
    Eventually, the factory will also be responsible for creating orders
    with the expected event format. Still, the factory will be able to
    create orders for any AMM, even those that haven't been created by it.
    
    ### How to test
    
    New unit tests.
    
    ---------
    
    Co-authored-by: mfw78 <53399572+mfw78@users.noreply.github.com>

--- src/ConstantProductFactory.sol
+++ src/ConstantProductFactory.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import {ISettlement} from "./ConstantProduct.sol";
+import {IConditionalOrder} from "lib/composable-cow/src/ComposableCoW.sol";
+
+import {ConstantProduct, ISettlement, GPv2Order} from "./ConstantProduct.sol";
 
 /**
  * @title CoW AMM Factory
@@ -22,5 +24,53 @@ contract ConstantProductFactory {
      */
     constructor(ISettlement _settler) {
         settler = _settler;
+    }
+
+    /**
+     * @notice This function exists to let the watchtower off-chain service
+     * automatically create AMM orders and post them on the orderbook. It
+     * outputs an order for the input AMM together with a valid signature.
+     * @dev Some parameters are unused as they refer to features of
+     * ComposableCoW that aren't implemented in this contract. They are still
+     * needed to let the watchtower interact with this contract in the same way
+     * as ComposableCoW.
+     * @param amm owner of the order.
+     * @param params `ConditionalOrderParams` for the order; precisely, the
+     * handler must be this contract, the salt can be any value, and the static
+     * input must be the current trading parameters of the AMM.
+     * @return order discrete order for submitting to CoW Protocol API
+     * @return signature for submitting to CoW Protocol API
+     */
+    function getTradeableOrderWithSignature(
+        ConstantProduct amm,
+        IConditionalOrder.ConditionalOrderParams calldata params,
+        bytes calldata, // offchainInput
+        bytes32[] calldata // proof
+    ) external view returns (GPv2Order.Data memory order, bytes memory signature) {
+        // This contract mimics the interface of ConditionalCoW to talk to the
+        // watchtower. In principle we'd still get a valid order if the handler
+        // is set to any address. However, we create conditional orders on this
+        // contract with this contract as the handler, so to make sure that the
+        // user isn't trying to forward this order to the incorrect contract,
+        // we revert with this error message.
+        if (address(params.handler) != address(this)) {
+            revert IConditionalOrder.OrderNotValid("can only handle own orders");
+        }
+
+        ConstantProduct.TradingParams memory tradingParams =
+            abi.decode(params.staticInput, (ConstantProduct.TradingParams));
+
+        // Check that `getTradeableOrderWithSignature` is being called with
+        // parameters that are currently enabled for trading on the AMM.
+        // If the parameters are different, this order can be deleted on the
+        // watchtower.
+        if (amm.hash(tradingParams) != amm.tradingParamsHash()) {
+            revert IConditionalOrder.OrderNotValid("invalid trading parameters");
+        }
+
+        // Note: the salt in params is ignored.
+
+        order = amm._getTradeableOrder(address(amm), tradingParams);
+        signature = abi.encode(order, tradingParams);
     }
 }
---
+++ test/ConstantProductFactory/ConstantProductFactoryTestHarness.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import {ConstantProductFactory, ConstantProduct, GPv2Order, IConditionalOrder} from "src/ConstantProductFactory.sol";
+
+import {ConstantProductTestHarness} from "test/ConstantProduct/ConstantProductTestHarness.sol";
+
+abstract contract ConstantProductFactoryTestHarness is ConstantProductTestHarness {
+    ConstantProductFactory internal constantProductFactory;
+
+    function setUp() public virtual override(ConstantProductTestHarness) {
+        super.setUp();
+        constantProductFactory = new ConstantProductFactory(solutionSettler);
+    }
+
+    // This function calls `getTradeableOrderWithSignature` while filling all
+    // unused parameters with arbitrary data.
+    function getTradeableOrderWithSignatureWrapper(
+        ConstantProduct amm,
+        ConstantProduct.TradingParams memory tradingParams
+    ) internal view returns (GPv2Order.Data memory order, bytes memory signature) {
+        IConditionalOrder.ConditionalOrderParams memory params = IConditionalOrder.ConditionalOrderParams(
+            IConditionalOrder(address(constantProductFactory)),
+            keccak256("ConstantProductFactoryTestHarness: some salt"),
+            abi.encode(tradingParams)
+        );
+        return constantProductFactory.getTradeableOrderWithSignature(
+            amm, params, bytes("ConstantProductFactoryTestHarness: offchainData"), new bytes32[](2)
+        );
+    }
+}
