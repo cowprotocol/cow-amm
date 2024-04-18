fdc5f9048e884fb5a7c290c76e4f3e33d2980af3
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-04-18 06:34:05 +0000

    Update e2e test to work with standalone AMM (#50)
    Part of making CoW AMM standalone
    (https://github.com/cowprotocol/cow-amm/pull/44).
    
    This is a sanity check to make sure that the changes so far work well
    together. We create an AMM, then create an order through the factory and
    settle it on CoW Protocol, making sure that the AMM balances have been
    updated.
    
    ### How to test
    
    This is just test code, no new features.
    
    ---------
    
    Co-authored-by: mfw78 <53399572+mfw78@users.noreply.github.com>

--- test/e2e/ConstantProduct.t.sol
+++ test/e2e/ConstantProduct.t.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import {
-    BaseComposableCoWTest, Safe, TestAccount, TestAccountLib
-} from "lib/composable-cow/test/ComposableCoW.base.t.sol";
+import {BaseComposableCoWTest, Safe, TestAccount} from "lib/composable-cow/test/ComposableCoW.base.t.sol";
 
-import {ConstantProduct, IConditionalOrder, GPv2Order, IERC20, ISettlement} from "src/ConstantProduct.sol";
+import {ConstantProduct, IERC20, GPv2Order, ISettlement} from "src/ConstantProduct.sol";
+import {ConstantProductFactory, IConditionalOrder} from "src/ConstantProductFactory.sol";
 import {UniswapV2PriceOracle, IUniswapV2Pair} from "src/oracles/UniswapV2PriceOracle.sol";
 import {Utils} from "test/libraries/Utils.sol";
 import {TestAccountHelper} from "test/libraries/TestAccountHelper.sol";
@@ -20,17 +19,15 @@ contract E2EConditionalOrderTest is BaseComposableCoWTest {
     IERC20 public DAI;
     IERC20 public WETH;
     IUniswapV2Pair pair;
-    ConstantProduct constantProduct;
+    ConstantProductFactory ammFactory;
     UniswapV2PriceOracle uniswapV2PriceOracle;
-    bytes32 domainSeparator;
 
     function setUp() public virtual override(BaseComposableCoWTest) {
         super.setUp();
         DAI = token0;
         WETH = token1;
-        constantProduct = new ConstantProduct(ISettlement(address(settlement)), DAI, WETH);
+        ammFactory = new ConstantProductFactory(ISettlement(address(settlement)));
         uniswapV2PriceOracle = new UniswapV2PriceOracle();
-        domainSeparator = composableCow.domainSeparator();
         IUniswapV2Factory uniswapV2Factory = UniswapV2Helper.deployUniswapV2FactoryAt(
             vm, Utils.addressFromString("E2EConditionalOrderTest UniswapV2 factory")
         );
@@ -53,27 +50,29 @@ contract E2EConditionalOrderTest is BaseComposableCoWTest {
     }
 
     function testE2ESettle() public {
-        (Safe amm, TestAccount[] memory owners) = deployAmmSafe();
-
-        ConstantProduct.TradingParams memory tradingParams = ConstantProduct.TradingParams(
-            0, uniswapV2PriceOracle, abi.encode(UniswapV2PriceOracle.Data(pair)), keccak256("order app data")
-        );
-        IConditionalOrder.ConditionalOrderParams memory params =
-            super.createOrder(constantProduct, keccak256("any salt"), abi.encode(tradingParams));
-        // Create the conditional order.
-        _create(address(amm), params, true);
+        ConstantProduct.TradingParams memory tradingParams = ConstantProduct.TradingParams({
+            minTradedToken0: 0,
+            priceOracle: uniswapV2PriceOracle,
+            priceOracleData: abi.encode(UniswapV2PriceOracle.Data(pair)),
+            appData: keccak256("order app data")
+        });
+        IConditionalOrder.ConditionalOrderParams memory params = IConditionalOrder.ConditionalOrderParams({
+            handler: IConditionalOrder(address(ammFactory)),
+            salt: keccak256("e2e:any salt"),
+            staticInput: abi.encode(tradingParams)
+        });
+        ConstantProduct amm = new ConstantProduct(ISettlement(address(settlement)), DAI, WETH);
 
         uint256 startAmountDai = 2_000 ether;
         uint256 startAmountWeth = 1 ether;
         // Deal the AMM reserves to the safe.
         deal(address(DAI), address(amm), startAmountDai);
         deal(address(WETH), address(amm), startAmountWeth);
-        // Authorise the vault relayer to pull the tokens from the safe.
-        amm.execCall(address(DAI), abi.encodeCall(DAI.approve, (address(relayer), type(uint256).max)), owners);
-        amm.execCall(address(WETH), abi.encodeCall(WETH.approve, (address(relayer), type(uint256).max)), owners);
+
+        amm.enableTrading(tradingParams);
 
         (GPv2Order.Data memory order, bytes memory sig) =
-            composableCow.getTradeableOrderWithSignature(address(amm), params, abi.encode(safe2), new bytes32[](0));
+            ammFactory.getTradeableOrderWithSignature(amm, params, hex"", new bytes32[](0));
 
         // The trade will be settled against bob.
         deal(address(DAI), bob.addr, startAmountDai);
@@ -93,16 +92,7 @@ contract E2EConditionalOrderTest is BaseComposableCoWTest {
         assertEq(startAmountWeth, endBalanceWeth + expectedDifferenceWeth);
         // Explicit price to see that it's reasonable
         assertEq(expectedDifferenceDai / expectedDifferenceWeth, 2_499);
-    }
 
-    function deployAmmSafe() internal returns (Safe amm, TestAccount[] memory owners) {
-        owners = new TestAccount[](2);
-        owners[0] = TestAccountLib.createTestAccount("owner 1");
-        owners[1] = TestAccountLib.createTestAccount("owner 2");
-
-        // The safe already sets the extensible fallback handler during its
-        // deployment.
-        amm = SafeHelper.createSafe(factory, singleton, owners, 1, address(eHandler));
-        amm.execCall(address(amm), abi.encodeCall(eHandler.setDomainVerifier, (domainSeparator, composableCow)), owners);
+        amm.disableTrading();
     }
 }
