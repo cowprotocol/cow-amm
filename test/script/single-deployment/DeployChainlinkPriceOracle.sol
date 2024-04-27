c713f50425fff05d50242860c06805eac84329af
mfw78 <mfw78@protonmail.com>
2024-04-27 05:04:23 +0000

    chore: deployment test

---
+++ test/script/single-deployment/DeployChainlinkPriceOracle.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import {Test} from "forge-std/Test.sol";
+
+import {DeployChainlinkPriceOracle} from "script/single-deployment/ChainlinkPriceOracle.s.sol";
+
+contract DeployChainlinkPriceOracleTest is Test {
+    DeployChainlinkPriceOracle script;
+
+    function setUp() public {
+        script = new DeployChainlinkPriceOracle();
+    }
+
+    function testDoesNotRevert() public {
+        script.run();
+    }
+}
