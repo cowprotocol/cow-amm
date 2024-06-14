677e124edd33052c7c14c2e6b0891241b1a0ae4b
mfw78 <mfw78@protonmail.com>
2024-06-14 08:55:56 +0000

    fix: compile and dep issues

--- src/ConstantProductHelper.sol
+++ src/ConstantProductHelper.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.24;
 
-import {IERC20} from "lib/openzeppelin/contracts/interfaces/IERC20.sol";
-import {GPv2Order} from "lib/composable-cow/src/BaseConditionalOrder.sol";
+import {IERC20, GPv2Order} from "lib/composable-cow/src/BaseConditionalOrder.sol";
 import {ConstantProduct} from "./ConstantProduct.sol";
 import {IPriceOracle} from "./interfaces/IPriceOracle.sol";
 
 contract ConstantProductHelper is IPriceOracle {
-    bytes32 public constant NUMERATOR_SLOT = keccak256("ConstantProductHelper.numerator");
-    bytes32 public constant DENOMINATOR_SLOT = keccak256("ConstantProductHelper.denominator");
+    // uint256(keccak256("ConstantProductHelper.numerator")) - 1
+    uint256 public constant NUMERATOR_SLOT = 0x223dcd62dbf383e930e6aa5d74a39264cb580dbc29ee32ecbfc9edd7179ff604;
+    // uint256(keccak256("ConstantProductHelper.denominator")) - 1
+    uint256 public constant DENOMINATOR_SLOT = 0x30843a766054dd7fc4b0ac76c32c0a5da91a64e646d782e95f94d37ff51d4178;
 
-    function events() external view returns (bytes32 createPool, bytes32 enablePool, bytes32 disablePool) {
+    function events() external pure returns (bytes32 createPool, bytes32 enablePool, bytes32 disablePool) {
         return (
             bytes32(keccak256("Deployed(address,address,address,address)")),
             bytes32(keccak256("TradingEnabled(bytes32,(uint256,address,bytes,bytes32))")),
@@ -21,8 +22,11 @@ contract ConstantProductHelper is IPriceOracle {
     /**
      * CoW AMM Pool helpers MUST return all tokens that may be traded on this pool
      */
-    function tokens(ConstantProduct pool) external view returns (IERC20[] memory) {
-        return [pool.token0(), pool.token1()];
+    function tokens(ConstantProduct pool) external view returns (IERC20[] memory tokens_) {
+        tokens_ = new IERC20[](2);
+        tokens_[0] = IERC20(pool.token0());
+        tokens_[1] = IERC20(pool.token1());
+        return tokens_;
     }
 
     /**
@@ -35,14 +39,13 @@ contract ConstantProductHelper is IPriceOracle {
      * @param data of arbitrary nature that may have been returned from
      * the 2nd argument to the pool enabling event. If arbitrary data isn't
      * supplied, supply `hex''`
-     * @returns The GPv2Order.Data struct for the CoW Protocol JIT order and
+     * @return The GPv2Order.Data struct for the CoW Protocol JIT order and
      * it's associated ERC-1271 signature.
      */
     function getOrder(ConstantProduct pool, uint256 numerator, uint256 denominator, bytes calldata data)
         external
-        view
         returns (GPv2Order.Data memory, bytes memory)
-    {
+    {        
         assembly ("memory-safe") {
             // Store the numerator and denominator in the slot
             tstore(NUMERATOR_SLOT, numerator)
@@ -52,12 +55,12 @@ contract ConstantProductHelper is IPriceOracle {
         ConstantProduct.TradingParams memory params = abi.decode(data, (ConstantProduct.TradingParams));
         ConstantProduct.TradingParams memory spoof = ConstantProduct.TradingParams({
             minTradedToken0: 0,
-            priceOracle: address(this),
+            priceOracle: IPriceOracle(address(this)),
             priceOracleData: hex"",
             appData: params.appData
         });
 
-        GPv2Order.Data memory order = pool.getOrder(spoof);
+        GPv2Order.Data memory order = pool.getTradeableOrder(spoof);
         return (order, abi.encode(order, params));
     }
 
