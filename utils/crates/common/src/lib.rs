3b70fc2632e5c6b166efeb3b46fe409f0a0bf3b8
mfw78 <mfw78@protonmail.com>
2024-06-26 09:03:42 +0000

    chore: ETH_RPC_URL env var consistency

--- utils/crates/common/src/lib.rs
+++ utils/crates/common/src/lib.rs
@@ -13,8 +13,8 @@ pub use contracts::IERC1271;
 pub use contracts::IERC20;
 
 pub fn rpc_url() -> Url {
-    std::env::var("RPC_URL")
-        .expect("Environment variable `RPC_URL` is not set")
+    std::env::var("ETH_RPC_URL")
+        .expect("Environment variable `ETH_RPC_URL` is not set")
         .parse()
-        .expect("Invalid URL")
+        .expect("Invalid ETH_RPC_URL")
 }
